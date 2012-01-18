dea67b9c9ef3689c1c608feaf262359ead0510bb
Jrabbit <jackjrabbit+github@gmail.com>
2012-01-17 19:10:33 -0500

    simple examples for day 12

---
+++ lectureCode/day12-objects/ex3_methods.py
@@ -0,0 +1,11 @@
+class Student(object):
+    def __init__(self, name="Tim"):
+        self.classes = ["CS 112", "Underwater Basket Weaving", "Fort Building 101", "Act like a penguin."]
+        self.name = name
+    def classes(self):
+        print "%s takes these classes:" % self.name
+        for i in self.classes:
+            print i
+
+student = Student()
+student.classes()
