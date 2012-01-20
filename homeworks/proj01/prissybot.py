90368fc7b458a0f76f33bec252897826eb9c97df
Alec Goebel <agoebel@swordandspade.com>
2012-01-20 15:51:12 -0500

    prissy bot homework written (proj 1).  fixes #3

---
+++ homeworks/proj01/prissybot.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""
+prissybot.py
+
+Prissy Bot
+========================
+Prissy bot, the rude chat bot, is just mean.  They do not listen, ask obnoxious questions, and says anything they like.
+"""
+
+# Step 1:
+# -----------------------
+# Program the following.  Make sure the user inputs their own name and 
+# responses.
+#
+# $ python prissybot.py
+# Enter your name:  Paul
+# 
+# PrissyBot: Hello there, Paul
+# Paul: hi bot
+# PrissyBot: You mean, "hi bot, sir!"
+
+
+
+# Step 2:
+# -----------------------
+# Flesh out prissy bot so they say more insulting things.
+#
+# Requirements:
+#  * 3 math problems
+#  * get and use lots of responses from the user
+#  * substituting variables into print strings
+#  * at least 3 insults
+
+
