9e3f32655d31eab1cae411919a5a9c0fa6b1aa08
Alec Goebel <agoebel@swordandspade.com>
2012-01-20 15:00:43 -0500

    problemset 2 final draft

--- homeworks/hw02-control_flow.py
+++ homeworks/hw02-control_flow.py
@@ -1,65 +1,85 @@
 #!/usr/bin/env python
+"""
+Problem Set 1
+=============================
+
+Solve these simple problems with conditionals and loops
+"""
+from hwtools import *
+
+#Section 1:  If Statements
+#==============================
+# 1) Get a number n from the input, is it even?
+# 2) If n is not even, double it.
+# 3) If n is divisible by 3, add four.
+# 4) What is n's letter grade value? (eg. 90-100 is A)
 
 
-Section 1:  If Statements
-==============================
 
-a = raw_input("Enter a number: ")
+# Section 2:  Loops
+# ==============================
 
-1) Get a number from the input, is it even?
-
-2) If it it is not even, double it.
-
-3) If it is divisible by 3, add four.
-
-3) Determine the letter grade value. (eg. 90-100 is A)
-
-
-
-Section 2:  Loops
-==============================
-
-1)  Get a multiple of three (n) from the input
-2)  Print a countdown from n to 0 by threes 
-    ex:  
-       9...
-       6...
-       3...
-       Done!
-3) If it is not a multiple of three, and is even, countdown from n to 0 by twos.
+# 1)  Get a multiple of three (n) from the input
+# 2)  Print a countdown from n to 0 by threes 
+#      ex:  
+#         9...
+#         6...
+#         3...
+#         Done!
+# 3) Get another number, if it is a multiple of 3, countdown by 
+#    threes again.  If it is not a multiple of 3 but is even, 
+#    countdown by twos.  Otherwise just countdown by one.
 
 
-Section 3:  Lists
-==============================
-nums = [ int(c.strip()) for c in raw_input("Enter a bunch of numbers, separated by commas").strip().split(",") ]
+# Section 3:  Lists
+# ==============================
+
+nums = input_nums()
+
+# 1)  How many numbers were entered?
+# 2)  Append 3 and 5 to nums then print it.
+# 3)  Remove the last element from nums
+# 4)  
 
 
-1)  How many numbers were entered?
-2)  Append 3 and 5 to nums.
-3)  Remove the last element from nums
+# Section 4:  For Loops
+# ==============================
+
+# 1) What is the sum of all the numbers in nums?
+# 2) Print every even number in nums
+# 3) Print the phrase "only even numbers" if nums only 
+#    contains even numbers, otherwise print "some odd numbers".
+# 4) 
 
 
-Section 4:  For Loops
-==============================
+# ADVANCED:
+# =====================================
+# 1) Multiply each value in nums by its index in the list.
+nums = input_nums()
 
-1) What is the sum of all the numbers in nums?
-2) Print "all even" if nums only contains even numbers, otherwise print "not all even" 
-3) Multiply each value in nums by its position in the list.
+# 2) You have n slices of cake. Ask if the user wants cake or death.
+#    If they answer incorrectly, ask again.  When they ask for cake, 
+#    give them one slice.  When there are no slices left and they 
+#    ask for cake, politely explain that you are out of cake and 
+#    ask again. On death, describe their demise.
+n = int(raw_input("How many slices of cake?  ")
 
+# 3) Make an ascii christmas tree (or a non-denominational decorative 
+#    holiday festival shubbery).  This tree has a star on the top, 
+#    has a height of "h" for branches, and the trunk is "t" tall.
+# 
+# An example tree:
+# 
+# Enter the height of the branches: 5
+# Enter the height of the trunk: 2
+#     *
+#     ^
+#    ^.^
+#   ^.^.^
+#  ^.^.^.^
+# ^.^.^.^.^
+#     |
+#     |
 
-Section 5:  Everything
-===============================
-
-1) You have n slices of cake. Ask if they want cake or death. If they choose neither, ask again. When they ask for cake, give them one slice. When there are no slices left, upon choosing cake, ask again. On death, describe their demise.
-
-2) Make an ascii christmas tree (or non-denominational decorate holiday festival shubbery).
-
-Enter the height of the tree: 4
-Enter the height of the trunk: 2
-   *
-   ^
-  ^.^
- ^.^.^
-^.^.^.^
-   |
-   | 
+h = int( raw_input("Enter the height of the branches:  ") )
+t = int( raw_input("Enter the height of the trunk:  ") )
