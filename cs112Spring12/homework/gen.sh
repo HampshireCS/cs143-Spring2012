99dfc18589e53b38eeaaa113ae9e98972bddb987
Alec Goebel <agoebel@swordandspade.com>
2012-01-24 17:12:34 -0500

    homework text

--- cs112Spring12/homework/gen.sh
+++ cs112Spring12/homework/gen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for n in {1..25}; do
-    echo "<HTML><HEAD><META HTTP-EQUIV=REFRESH CONTENT=\"1; URL=http://github.com/HampshireCS/cs112-spring12/tree/master/hw$n\"></HEAD></HTML>" > hw$n.html
+    printf "<HTML><HEAD><META HTTP-EQUIV=REFRESH CONTENT=\"1; URL=http://github.com/HampshireCS/cs112-spring12/tree/master/hw%02d\"></HEAD></HTML>"  $n> hw$n.html
 done
