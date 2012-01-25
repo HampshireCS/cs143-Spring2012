69536014e9c56be904fc9e8709636a67b01c94b0
Alec Goebel <agoebel@swordandspade.com>
2012-01-25 10:06:05 -0500

    homework pages and gen

--- cs112Spring12/homework/gen.sh
+++ cs112Spring12/homework/gen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for n in {1..25}; do
-    printf "<HTML><HEAD><META HTTP-EQUIV=REFRESH CONTENT=\"1; URL=http://github.com/HampshireCS/cs112-spring12/tree/master/hw%02d\"></HEAD></HTML>"  $n> hw$n.html
+    printf "<HTML><HEAD><META HTTP-EQUIV=REFRESH CONTENT=\"1; URL=http://github.com/HampshireCS/CS112-Spring2012/tree/master/hw%02d\"></HEAD></HTML>"  $n> hw$n.html
 done
