85c5723b36d022ad9753758935fb35a9666b5a05
Jrabbit <jackjrabbit+github@gmail.com>
2012-01-31 23:26:29 -0500

    fixord

--- tools/octoforks.py
+++ tools/octoforks.py
@@ -2,11 +2,11 @@ import requests
 import json
 import getpass
 
-REPO_NAME = "bootstrap"
-REPO_ORG = "twitter"
+# REPO_NAME = "bootstrap"
+# REPO_ORG = "twitter"
 
-#REPO_NAME = "cs112-Spring2012"
-#REPO_ORG = "HampshireCS112"
+REPO_NAME = "cs112-Spring2012"
+REPO_ORG = "HampshireCS"
 
 def do_gh(url, method="post", **kw):
     method = getattr(requests, method)
@@ -14,9 +14,11 @@ def do_gh(url, method="post", **kw):
     r.raise_for_status()
     return r.content
 
-fork_data = json.loads(requests.get('https://api.github.com/repos/%s/%s/forks' % (REPO_ORG, REPO_NAME).content)
+fork_data = json.loads(requests.get('https://api.github.com/repos/%s/%s/forks' % (REPO_ORG, REPO_NAME)).content)
 
 urls = [{"url": x['git_url'],"username": x['owner']['login'] } for x in fork_data]
 
-for d in urls:
-    os.system("git clone %s %s" % (d['url'], d['username']))
+print len(urls)
+# for d in urls:
+#     os.system("git clone %s %s" % (d['url'], d['username']))
+
